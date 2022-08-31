35ac50df59519d182a56533c9fd1a73403fd9b6e
Kishore Kumar <84456929+mouse-rider@users.noreply.github.com>
2022-08-31 12:29:53 +0530

    Add files via upload

--- Django/products/views.py
+++ Django/products/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def home_view(request):
     return HttpResponse("<h1>Welcome to Django Tutorial</h1>");
 
+def product_view(request):
+    return HttpResponse("<h1>project</h1>");
+
+def contact_view(request):
+    return HttpResponse("<h1>contact</h1>");
+
+def about_view(request):
+    return HttpResponse("<h1>about</h1>");
